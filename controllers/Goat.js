a08a192c5a46e98f1df7015d26fd1f2969a33210
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-04-03 00:01:14 -0500

    update

--- controllers/Goat.js
+++ controllers/Goat.js
@@ -1,8 +1,16 @@
 var Goat= require('../models/Goat');
 // List of all Costumes
-exports.Goat_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Goat list');
-};
+exports.Goat_list = async function(req, res) {
+    try{
+    theCostumes = await Goat.find();
+    res.send(theCostumes);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // for a specific Costume.
 exports.Goat_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Goat detail: ' + req.params.id);
@@ -19,3 +27,5 @@ res.send('NOT IMPLEMENTED: Goat delete DELETE ' + req.params.id);
 exports.Goat_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Goat update PUT' + req.params.id);
 };
+
+
--- routes/resource.js
+++ routes/resource.js
@@ -8,13 +8,13 @@ var costume_controller = require('../controllers/Goat');
 router.get('/', api_controller.api);
 /// COSTUME ROUTES ///
 // POST request for creating a Costume.
-router.post('/Goats', costume_controller.Goat_create_post);
+router.post('/Goat', costume_controller.Goat_create_post);
 // DELETE request to delete Costume.
-router.delete('/Goats/:id', costume_controller.Goat_delete);
+router.delete('/Goat/:id', costume_controller.Goat_delete);
 // PUT request to update Costume.
-router.put('/Goats/:id', costume_controller.Goat_update_put);
+router.put('/Goat/:id', costume_controller.Goat_update_put);
 // GET request for one Costume.
-router.get('/Goats/:id', costume_controller.Goat_detail);
+router.get('/Goat/:id', costume_controller.Goat_detail);
 // GET request for list of all Costume items.
-router.get('/Goats', costume_controller.Goat_list);
+router.get('/Goat', costume_controller.Goat_list);
 module.exports = router;
