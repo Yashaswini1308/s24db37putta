5ce7e86503bfb95ac9a1d24610bdb40d864bcd56
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-04-03 23:19:16 -0500

    update

--- controllers/Goat.js
+++ controllers/Goat.js
@@ -15,10 +15,10 @@ exports.Goat_list = async function(req, res) {
 exports.Goat_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Goat detail: ' + req.params.id);
 };
-// // Handle Costume create on POST.
-// exports.Goat_create_post = function(req, res) {
-// res.send('NOT IMPLEMENTED: Goat create POST');
-// };
+// Handle Costume create on POST.
+exports.Goat_create_post = function(req, res) {
+res.send('NOT IMPLEMENTED: Goat create POST');
+};
 // Handle Costume delete from on DELETE.
 exports.Goat_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Goat delete DELETE ' + req.params.id);
