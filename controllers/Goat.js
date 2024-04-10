293835ed68f7458ef692b222ad9c49aae59a2056
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-04-10 16:43:11 -0500

    updated

--- controllers/Goat.js
+++ controllers/Goat.js
@@ -1,5 +1,5 @@
 var Goat= require('../models/Goat');
-// List of all Goats
+// List of all Goat
 exports.Goat_list = async function(req, res) {
     try{
     theCostumes = await Goat.find();
@@ -27,6 +27,12 @@ exports.Goat_detail = async function(req, res) {
 exports.Goat_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: Goat create POST');
 };
+
+// Handle Goat update form on PUT.
+exports.Goat_update_put = function(req, res) {
+  res.send('NOT IMPLEMENTED: Goat update PUT' + req.params.id);
+  };
+
 // Handle Goat delete from on DELETE.
 exports.Goat_delete = async function(req, res) {
     console.log("delete " + req.params.id)
@@ -54,6 +60,21 @@ res.send(`{'error': '${err}'}`);
 }
 }; 
 
+// Handle building the view for creating a Goat.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Goat_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Goatcreate', { title: 'Goat Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+
     
 // Handle Goat update form on PUT.
 exports.Goat_update_put = async function(req, res) {
@@ -62,10 +83,9 @@ ${JSON.stringify(req.body)}`)
 try {
 let toUpdate = await Goat.findById( req.params.id)
 // Do updates of properties
-if(req.body.Goat_type)
-toUpdate.Goat_color= req.body.Goat_color;
+if(req.body.Goat_color) toUpdate.Goat_color = req.body.Goat_color;
 if(req.body.Goat_breed) toUpdate.Goat_breed = req.body.Goat_breed;
-if(req.body.Goat_price) toUpdate.size = req.body.Goat_price;
+if(req.body.Goat_price) toUpdate.Goat_price = req.body.Goat_price;
 let result = await toUpdate.save();
 console.log("Sucess " + result)
 res.send(result)
@@ -106,6 +126,35 @@ exports.Goat_view_all_Page = async function(req, res) {
     }
     };
     
+// Handle a delete one view with id from query
+exports.Goat_delete_Page = async function(req, res) {
+  console.log("Delete view for id " + req.query.id)
+  try{
+  result = await Goat.findById(req.query.id)
+  res.render('Goatdelete', { title: 'Goat Delete', toShow:
+  result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  };
+  
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.Goat_update_Page = async function(req, res) {
+  console.log("update view for item "+req.query.id)
+  try{
+  let result = await Goat.findById(req.query.id)
+  res.render('Goatupdate', { title: 'Goat Update', toShow: result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  };
+  
     
 
 
