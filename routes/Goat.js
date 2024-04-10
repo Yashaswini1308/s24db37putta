f693be99d34db0cbb58b55052f5710f753845a8e
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-04-10 16:43:53 -0500

    Modified

--- routes/Goat.js
+++ routes/Goat.js
@@ -16,4 +16,15 @@ router.get('/', Goat_controlers.Goat_view_all_Page );
 /* GET detail Goat page */
 router.get('/detail', Goat_controlers.Goat_view_one_Page);
 
+/* GET create Goat page */
+router.get('/create', Goat_controlers.Goat_create_Page);
+
+/* GET create update page */
+router.get('/update', Goat_controlers.Goat_update_Page);
+
+/* GET delete Goat page */
+router.get('/delete', Goat_controlers.Goat_delete_Page);
+
+
+
 module.exports = router;
