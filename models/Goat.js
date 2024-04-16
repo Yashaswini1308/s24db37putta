4d8a3bc6b7e779f1ef1e4ad95a501f06b94ec434
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-04-16 16:59:46 -0500

    Bonus

--- models/Goat.js
+++ models/Goat.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const GoatSchema = mongoose.Schema({
-Goat_color: String,
+Goat_color: {type:String,length:20},
 Goat_breed: String,
-Goat_price: Number
+Goat_price:{type:Number,min:2000,max:250000}
 })
 module.exports = mongoose.model("Goat",GoatSchema)
